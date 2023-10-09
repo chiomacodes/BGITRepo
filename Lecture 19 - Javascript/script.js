508addb1057b5a35d9947da4ee65ff14d66cd4ff
chiomacodes <chiomawriting@gmail.com>
2023-10-09 19:49:22 +0100

    js calculator attempt

---
+++ Lecture 19 - Javascript/script.js
@@ -0,0 +1,11 @@
+let num1 = 5;
+let num2 = 8;
+sum = (num1 + num2);
+difference = (num1 - num2);
+product = (num1 * num2);
+quotient = (num1 / num2);
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
