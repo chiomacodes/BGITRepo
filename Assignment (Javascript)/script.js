bfd01168d7e499cc5910f43864ae8aa92dc0cb07
chiomacodes <chiomawriting@gmail.com>
2023-10-22 23:58:24 +0100

    Javascript Assignment

---
+++ Assignment (Javascript)/script.js
@@ -0,0 +1,17 @@
+<script>
+    document.getElementById("toBlue").onclick = function () {
+            document.getElementById("output_text").style.color = 'blue';
+    }
+
+            document.getElementById("toPink").onclick = function () {
+            document.getElementById("output_text").style.color = 'pink';
+            }
+
+            document.getElementById("toGreen").onclick = function () {
+            document.getElementById("output_text").style.color = 'green';
+            }
+
+            document.getElementById("toYellow").onclick = function () {
+            document.getElementById("output_text").style.color = 'yellow';
+            }
+</script>
